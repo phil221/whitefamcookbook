1303e8a31221302d9eb828416267bed228bd0b57
Phil Meckley <phil@immaculatastudios.com>
2023-12-29 14:04:01 -0500

    add authors enum to recipe collection schema

---
+++ src/constants/recipeAuthors.ts
@@ -0,0 +1,23 @@
+const recipeAuthors = [
+  "the 'Matriarch'",
+  "Sara Burch",
+  "Clare Daly",
+  "Elizabeth Daly",
+  "Liz Dillon",
+  "Mary Dooley",
+  "Clare McCormack",
+  "Molly Smillie",
+  "Jeannie Stirton",
+  "Jessica Thornton",
+  "Katie Thornton",
+  "Cathy White",
+  "Deb White",
+  "Isabel White",
+  "Margaret White",
+  "Meg White",
+  "Rose White",
+  "Sue White",
+  "see recipe notes",
+] as const;
+
+export default recipeAuthors;
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,14 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
 import recipeCategories from '../constants/recipeCategories';
+import recipeAuthors from '../constants/recipeAuthors';
 
 // 2. Define your collection(s)
 const recipeCollection = defineCollection({ 
     type: "content",
     schema: z.object({
         name: z.string(),
-        author: z.string(),
+        author: z.enum(recipeAuthors),
         category: z.enum(recipeCategories),
         instructions: z.string(),
         image: z.string().optional()
