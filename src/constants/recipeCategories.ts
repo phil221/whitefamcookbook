e7a777a3a70f52c27ca92fba333b401f100d5951
Phil Meckley <phil@immaculatastudios.com>
2023-12-29 13:47:03 -0500

    add categories file

---
+++ src/constants/recipeCategories.ts
@@ -0,0 +1,9 @@
+export const recipeCategories = [
+    "Appetizers and Party Food",
+    "Breads",
+    "Muffins, Rolls and other Breakfast Fare",
+    "Entrees",
+    "Sides and Salads",
+    "Cookies",
+    "Yummy Deserts",
+] as const;
